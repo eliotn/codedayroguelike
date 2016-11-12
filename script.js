7d2e3b47e9c20075b39ba6296b3a857b8f9d6f7b
Eliot Glairon <ejg_dnd@yahoo.com>
2016-11-12 23:54:08 +0000

    fill screen

--- script.js
+++ script.js
@@ -1,25 +1,44 @@
+//Rogue Like For Code Day
+var clearStuff = document.getElementById("clear");
 var myButton = document.getElementById("clickButton");
-var myText = document.getElementById("helloText");
+var tileimage;
 
-myButton.addEventListener('click', doSomething, false)
+var imageObj = new Image(256, 256);
+
+imageObj.onload = function() {
+    myButton.addEventListener('click', doSomething, false)
+};
+imageObj.src = './images/df.png';
 
 function doSomething() {
-	myText.textContent = "Rogue Like";
+    clearStuff.style.display = 'none';
 	document.getElementById("game").innerHTML = 
-	"<canvas width=600 height=500 id='gamecanvas'></canvas>";
+	"<canvas width=640 height=480 id='gamecanvas'></canvas>";
 	setInterval(draw, 50);
 }
 
-var i = 1;
+
+var tiles = [];
+for (var x = 0; x < 40; x++) {
+    
+    tilelist = [];
+    for (var y = 0; y < 30; y++) {
+        tilelist.push((x*y + 2)%256);
+    }
+    tiles.push(tilelist);
+}
 
 function draw() {
+    console.log(tiles);
     var cv = document.getElementById("gamecanvas");
     var ctx = cv.getContext("2d");
     ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height);
-    ctx.beginPath();
-    ctx.moveTo(i, i);
-    ctx.lineTo(10, 50);
-    ctx.stroke();
-    i += 1;
+    for (var y = 0; y < 30; y++) {
+        for (var x = 0; x < 40; x++) {
+            ctx.drawImage(imageObj, 0, 16, 16, 16, 16*x, 16*y, 16, 16);
+        }
+    }
+    ctx.drawImage(imageObj, 0, 16, 16, 16, 0, 0, 16, 16);
+    
     
 }
